4ec8d7980ada419eb184cfdcca5eed1f187df732
Wang Yalong <wyl.00@163.com>
2018-01-26 14:33:26 +0800

    增加User-Agent

--- myUrllib/httpUtils.py
+++ myUrllib/httpUtils.py
@@ -1,7 +1,29 @@
 # -*- coding: utf8 -*-
 import datetime
 import requests
+import random
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+
+USER_AGENTS = [ 
+ "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", 
+ "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)", 
+ "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", 
+ "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)", 
+ "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)", 
+ "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)", 
+ "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)", 
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)", 
+ "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6", 
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1", 
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0", 
+ "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5", 
+ "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6", 
+ "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11", 
+ "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20", 
+ "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52", 
+]
 
 class HTTPClient(object):
 
@@ -22,33 +44,36 @@ class HTTPClient(object):
             "Referer": "https://kyfw.12306.cn/otn/login/init",
             "Accept": "*/*",
         }
+        
+        agent = random.choice(USER_AGENTS)
+        add_header.update({'User-Agent' : agent})
         self.session.headers.update(add_header)
 
-    def get(self, url, proxy=None, **kwargs):
-        if proxy:
-            proxies = {"http": proxy}
-        else:
-            proxies = ""
-        response = self.session.request(method="GET",
-                                        url=url,
-                                        proxies=proxies,
-                                        **kwargs)
-        if response.status_code == 200:
-            return response.content
-        else:
-            print(("请求失败。{0}".format(response)))
+    # def get(self, url, proxy=None, **kwargs):
+    #     if proxy:
+    #         proxies = {"http": proxy}
+    #     else:
+    #         proxies = ""
+    #     response = self.session.request(method="GET",
+    #                                     url=url,
+    #                                     proxies=proxies,  verify=False,
+    #                                     **kwargs)
+    #     if response.status_code == 200:
+    #         return response.content
+    #     else:
+    #         print(("请求失败。{0}".format(response)))
 
-    def post(self, url, data=None, proxy=None, **kwargs):
-        if proxy:
-            proxies = {"http": proxy}
-        else:
-            proxies = ""
-        response = self.session.request(method="POST",
-                                        url=url,
-                                        data=data,
-                                        proxies=proxies,
-                                        **kwargs)
-        if response.status_code == 200:
-            return response.content
-        else:
-            print(("请求失败。{0}".format(response)))
+    # def post(self, url, data=None, proxy=None, **kwargs):
+    #     if proxy:
+    #         proxies = {"http": proxy}
+    #     else:
+    #         proxies = ""
+    #     response = self.session.request(method="POST",
+    #                                     url=url,
+    #                                     data=data,
+    #                                     proxies=proxies ,verify=False,
+    #                                     **kwargs)
+    #     if response.status_code == 200:
+    #         return response.content
+    #     else:
+    #         print(("请求失败。{0}".format(response)))

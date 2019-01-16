810dbfadeaa66db35372071c2b5b0deae22e8f6f
wenxianping <931128603@qq.com>
2019-01-16 13:48:27 +0800

    增加server酱测试方法

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 from config.emailConf import sendEmail
+from config.pushbearConf import sendPushBear
 from init import select_ticket_info
 
 
@@ -11,6 +12,11 @@ def Email():
     sendEmail(u"订票小助手测试一下")
 
 
+def PushbearConf():
+    sendPushBear("订票小助手测试一下")
+
+
 if __name__ == '__main__':
     run()
     # Email()
+    # PushbearConf()

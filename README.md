b6e1f6d0719a33399d3148564fc102a71599c300
文贤平 <jim.wen@huolala.cn>
2019-09-03 15:06:37 +0800

    update version 1.1.103

--- README.md
+++ README.md
@@ -95,10 +95,6 @@
    - 如果下载验证码过期或者下载失败的问题，应该是12306封ip的策略，多重试几次，12306现在封服务器(阿里云和腾讯云)ip比较严重，尽量不要放在服务器里面
    - 目前12306对服务器ip比较敏感，大家还是在自己家里挂着吧
    - 自动更换ip软件目前已支持TPLINK和小米路由器，只限家庭网络[点我跳转](https://github.com/testerSunshine/AutoRouterIP)
-   - 登录302问题目前的解决办法为使用selenium打开浏览器获取devicesID之后，在登录，目前可以100%一次登录成功，但是不能登录次数过多，否则也是会302，测试官网的登录也会302（连自己的都不放过）
-     - selenium教程[戳我](https://www.jianshu.com/p/8ec70859ae03)
-     - windows的chromedriver需放置在chrome安装路径下，才可启动浏览器
-     - chromeDriver下载地址[戳我](http://chromedriver.storage.googleapis.com/index.html)
 
 #### 感谢一下小伙伴对本项目提供的帮助
    - @sun7127@126.com
--- TickerConfig.py
+++ TickerConfig.py
@@ -14,20 +14,20 @@ J_Z_PARAM = "2019-09-28#22#59"
 
 # 出发日期(list) "2018-01-06", "2018-01-07"
 STATION_DATES = [
-    "2019-10-01"
+    ""
 ]
 
 # 填入需要购买的车次(list)，"G1353"
 STATION_TRAINS = [
-    "G6142",
-    "G6174",
+    "",
+    "",
 ]
 
 # 出发城市，比如深圳北，就填深圳就搜得到
-FROM_STATION = "深圳北"
+FROM_STATION = ""
 
 # 到达城市 比如深圳北，就填深圳就搜得到
-TO_STATION = "隆回"
+TO_STATION = ""
 
 # 座位(list) 多个座位ex:
 # "商务座",
@@ -40,7 +40,7 @@ TO_STATION = "隆回"
 # "无座",
 # "动卧",
 SET_TYPE = [
-    "二等座",
+    "",
 ]
 
 # 当余票小于乘车人，如果选择优先提交，则删减联系人和余票数一致在提交
@@ -51,13 +51,12 @@ IS_MORE_TICKET = True
 # - "张三"
 # - "李四"
 TICKET_PEOPLES = [
-    "文贤平",
-    "李梦云",
+    "",
 ]
 
 # 12306登录账号
-USER = "931128603@qq.com"
-PWD = "QWERTY"
+USER = ""
+PWD = ""
 
 # 加入小黑屋时间默认为5分钟，此功能为了防止僵尸票导致一直下单不成功错过正常的票
 TICKET_BLACK_LIST_TIME = 5
@@ -80,10 +79,10 @@ IS_AUTO_CODE = True
 #  host: "smtp.qq.com"
 EMAIL_CONF = {
     "IS_MAIL": True,
-    "email": "931128603@qq.com",
-    "notice_email_list": "931128603@qq.com",
-    "username": "931128603@qq.com",
-    "password": "lwvgfrcydzyvbfjf",
+    "email": "",
+    "notice_email_list": "",
+    "username": "",
+    "password": "",
     "host": "smtp.qq.com",
 }
 
@@ -115,7 +114,7 @@ IS_PROXY = 0
 OPEN_TIME = "13:00:00"
 
 # chromeDriver路径,下载地址http://chromedriver.storage.googleapis.com/index.html
-CHROME_PATH = "/Users/wenxianping/Downloads/chromedriver"
+# CHROME_PATH = "/Users/wenxianping/Downloads/chromedriver"
 
 PASSENGER_TICKER_STR = {
     '一等座': 'M',
@@ -130,4 +129,4 @@ PASSENGER_TICKER_STR = {
 }
 
 # 软件版本
-RE_VERSION = "1.1.102"
+RE_VERSION = "1.1.103"

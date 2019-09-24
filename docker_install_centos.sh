b6b3114d28252ff894daee8b1983abe49078a3a3
PythonNM1 <36836973+PythonNM1@users.noreply.github.com>
2019-09-24 20:14:35 +0800

    mackecache  -->  makecache
    单词拼写有误

--- docker_install_centos.sh
+++ docker_install_centos.sh
@@ -22,7 +22,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
 
-sudo yum mackecache fast 
+sudo yum makecache fast 
 sudo yum -y install docker-ce
 sudo systemctl start docker
 

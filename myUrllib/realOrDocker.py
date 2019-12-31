6bd9e2e929f8356a655c42949158ece66741f9b4
任昌伟 <small_pupil@126.com>
2019-12-31 10:33:51 +0800

    fix : 逻辑修改

--- myUrllib/realOrDocker.py
+++ myUrllib/realOrDocker.py
@@ -7,22 +7,22 @@ path_self = "/proc/self/cgroup"
 
 def verify_docker():
     """
-    docker 环境下返回False
+    docker 环境下返回True
     :return:
     """
     if os.path.isfile(path_1):
         with open(path_1, "r") as f:
             for line in f.readlines():
                 if "docker" in line:
-                    return False
+                    return True
                 else:
                     continue
     elif os.path.isfile(path_self):
         with open(path_self, "r") as f:
             for line in f.readlines():
                 if "docker" in line:
-                    return False
+                    return True
                 else:
                     continue
     else:
-        return True
+        return False
